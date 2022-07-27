bff807d3ca6ef5b103ea799903088f85fcb0c32b
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-07-27 10:40:51 -0300

    Atividade Banco de dados relacional

--- Exercicio 3.sql
+++ Exercicio 3.sql
@@ -1,3 +1,11 @@
+/* Crie um banco de dados para um registro de uma escola, onde o sistema trabalhará com as informações dos estudantes deste registro dessa escola. 
+Crie uma tabela estudantes e utilizando a habilidade de abstração e determine 5 atributos relevantes dos estudantes para se trabalhar com o serviço dessa escola.
+Insira nesta tabela no mínimo 8 dados (registros).
+Faça um SELECT que retorne todes o/a(s) estudantes com a nota maior do que 7.0.
+Faça um SELECT que retorne todes o/a(s) estudantes com a nota menor do que 7.0.
+Ao término atualize um registro desta tabela através de uma query de atualização.
+Salve todas as queries para cada um dos requisitos do exercício em um único script (arquivo .SQL) e coloque no seu Github pessoal, no repositório que você criou sobre Banco de dados.
+ */
 CREATE DATABASE db_escola;
 USE db_escola;
 
@@ -11,11 +19,14 @@ CREATE TABLE tb_estudantes(
 );
 
 SELECT * FROM tb_estudantes;
- INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Arthur", true, 17, "Av. São Paulo", 6.5);
- INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Amanda", true, 16, "Rua 13 de maio", 7.7);
- INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Alex", true, 15, "Av. Dois Rios", 8.0);
- INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Thaisa", true, 18, "Rua Tirandentes", 9.9);
-
-
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Arthur", true, 17, "Av. São Paulo", 6.5);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Amanda", true, 16, "Rua 13 de maio", 7.7);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Alex", true, 15, "Av. Dois Rios", 8.0);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Thaisa", true, 18, "Rua Tirandentes", 9.9);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Carlos", true, 17, "Rua Tirandentes", 7.0);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Maria", true, 12, "Rua Tirandentes", 6.5);
+INSERT INTO tb_estudantes(nome_estudante, ativo, idade, endereco, notas) VALUES ("Lurdes", true, 11, "Rua Tirandentes", 7.4);
  
- 
+ SELECT * FROM tb_estudantes WHERE notas > 7;
+ SELECT * FROM tb_estudantes WHERE notas < 7;
+ SELECT * FROM tb_estudantes;
